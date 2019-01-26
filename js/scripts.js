1f41bec39d4cc1bfae825d30a19ee603e0d85a21
Tracy Reith <treith@treith.com>
2019-01-25 16:52:31 -0800

    Revert code to a good state - not able to get code to return user name in place of Dave

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-// * BUSINESS LOGIC
+// ******** BUSINESS LOGIC ************
 
 // *GETS NUMBER, BREAKS INTO COMPONENT NUMBERS
 function getNumber(userInput) {
@@ -10,22 +10,24 @@ function getNumber(userInput) {
   }
   return nums;
 };
+
+// * ATTEMPT TO REPLACE 'DAVE' WITH USER NAME -- NEEDS WORK
+// function replaceName(userName) {
+//   // var name;
+//   if (userName !== "") {
+//     return name = userName;
+//   } else {
+//     return name = "Dave";
+//   }
+// };
+
 // *TRANSFORMS NUMBERS: 1 = BEEP, 2 = BOOP, 3 = I'M SORRY DAVE
 function beepboop(userInput) {
   var nums = getNumber(userInput);
-  // var userName = "Tracy"
-  console.log(userName);
-  debugger;
-  function getName(userName) {
-    if (userName !== "") {
-        return name = userName;
-      } else {
-        return name = "Dave";
-      }
-    return name;
-    };
+  // console.log(userName);
   // debugger;
   var transformNums = nums.map(function(num) {
+    name = "Dave"
     if (num.includes("3")) {
       num = " I'm sorry, " + name + ". I'm afraid I can't do that.";
     } else if (num.includes("2")) {
@@ -40,19 +42,17 @@ function beepboop(userInput) {
   return transformNums;
 };
 
-
-
-
-// * UI LOGIC
+// ********* UI LOGIC ************
 $(document).ready(function() {
   $("form#beepboop").submit(function(event){
     event.preventDefault();
 
     var userInput = parseInt($("input#userInput").val());
     var userName = $("#userName").val();
+    console.log(userName);
+
     var output = beepboop(userInput);
     console.log(output);
-    // var output = "text goes here";
     $("p.result").text(output);
   });
 
